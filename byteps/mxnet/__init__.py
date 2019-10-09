a8b5083fcbc99738639deda09e825bf0ba4ce06f
joapolarbear <540358178@qq.com>
2019-10-09 11:48:26 +0800

    Debug nx.write_gml, should add stringizer to convert None str to str

--- byteps/mxnet/__init__.py
+++ byteps/mxnet/__init__.py
@@ -114,7 +114,7 @@ class Recorder(object):
         self.time_dict["traceEvents"] += rst_traces["traceEvents"]
         with open(self.trace_path, 'w') as f:
             json.dump(self.time_dict, f, indent=4)
-        nx.write_gml(self.dag, self.trace_dir + "dag.gml")
+        nx.write_gml(self.dag, self.trace_dir + "dag.gml", lambda x: str(x))
         log("Stop tracing, output trace: %s" % self.trace_path)
         """ clear the time dict after save it"""
         self.time_dict = None
